5aa8d51d476d47c422d9c5085449c52e836395c0
ccaccachahuafeli <ccaccachahuafeliciana@gmail.com>
2024-10-23 13:12:40 -0500

    feet:tipos de funciones por su notacio
    funciones como valor,de flecha

--- 4.funciones/funciones.js
+++ 4.funciones/funciones.js
@@ -1,5 +1,9 @@
 // ejercicio
 // crear una funcion que me de un saludo de bienvenida
-function saludo(){
+/**function saludo(){
     console.log("bienvenido")
+}*/
+let saludo=function(){
+    console.log(bienvenido)
 }
+saludo()
